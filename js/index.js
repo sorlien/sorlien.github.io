e217f2cc8dcda7cd3a9660ccdb38ddd29c5f2671
sorlien <amasoreg@gmail.com>
2019-09-08 17:41:41 +0200

    Add test files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var yourNavigation = $("#navbar");
+    stickyDiv = "sticky";
+    yourHeader = $('.bg').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > yourHeader ) {
+    yourNavigation.addClass(stickyDiv);
+  } else {
+    yourNavigation.removeClass(stickyDiv);
+  }
+});
