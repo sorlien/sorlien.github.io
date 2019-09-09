8c4af2ab3941c80347c060f792432072b4301fd6
sorlien <amasoreg@gmail.com>
2019-09-09 22:31:27 +0200

    Navigation scroll background with border box
    Updated the colour of the navigation background on scroll and added bording box

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {
-    document.getElementById("navbar").style.backgroundColor = "black";
+    document.getElementById("navbar").style.backgroundColor = "#fbf4f5";
+    document.getElementById("navbar").style.boxShadow = "0 0 .5em rgba(0, 0, 0, .5)";
+
   } else {
     document.getElementById("navbar").style.backgroundColor = "rgba(0,0,0,0)";
+    document.getElementById("navbar").style.boxShadow = "0 0 0 rgba(0, 0, 0, 0)";
+
   }
 }
-
